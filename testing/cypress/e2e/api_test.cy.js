cba815c9f118ba1a49e65ea7f7ec9aa4693a6901
Betza PM <betzapamolina@gmail.com>
2026-06-05 10:09:24 -0600

    :sparkles: feat: agregar pruebas de API, escaneo de seguridad y documentacion

---
+++ testing/cypress/e2e/api_test.cy.js
@@ -0,0 +1,14 @@
+//test simple para API pública
+describe('Ecosistema Suite - Pruebas de API', () => {
+  it('Verificar endpoint público exitosamente', () => {
+    cy.request('https://jsonplaceholder.typicode.com/posts/1').should((response) => {
+      expect(response.status).to.eq(200);
+    });
+  });
+});describe('Ecosistema Suite - Pruebas de API', () => {
+  it('Verificar endpoint público exitosamente', () => {
+    cy.request('https://jsonplaceholder.typicode.com/posts/1').should((response) => {
+      expect(response.status).to.eq(200);
+    });
+  });
+});
